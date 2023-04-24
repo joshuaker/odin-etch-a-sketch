ffff506d62161f73914538b6b991ef14a7972196
Joshua K <the.joshua.ker@gmail.com>
2023-04-24 15:07:42 +0800

    implemented smart width and height
    added javascript for gridlength/sides for square sides
    instead of hardcoding it into styles

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let sides = 16;
 let squareNum = sides * sides;
 const grid = document.querySelector("#grid-container");
+// grid width/height of 960px divided by # of sides
+const gridStyle = getComputedStyle(grid);
+const gridLength = gridStyle.width;
+const squareLength = `${parseInt(gridLength)/sides}px`
 
 while (squareNum > 0) {
-    console.log(squareNum);
     squareNum--;
     let div = document.createElement("div");
     div.classList.add("square");
+    div.style.width = squareLength;
+    div.style.height = squareLength;
     grid.append("", div)
 }
