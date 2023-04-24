cd9777eb14a00bdcbfa925ffcb078d0916ba3790
Joshua K <the.joshua.ker@gmail.com>
2023-04-24 14:56:01 +0800

    finished initial script for 16x16 squares
    - had to defer the script read for append to process grid
    - needed to remember borderbox for correct fill

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let sides = 16;
 let squareNum = sides * sides;
-const grid = document.querySelector("#grid-container")
+const grid = document.querySelector("#grid-container");
 
 while (squareNum > 0) {
+    console.log(squareNum);
     squareNum--;
     let div = document.createElement("div");
     div.classList.add("square");
